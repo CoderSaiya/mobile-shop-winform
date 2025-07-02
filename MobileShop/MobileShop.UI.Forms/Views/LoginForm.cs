124514219f6ca5127f445135b737d760519e548b
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-02 11:19:15 +0700

    update login form UI

---
+++ MobileShop/MobileShop.UI.Forms/Views/LoginForm.cs
@@ -0,0 +1,30 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace MobileShop.UI.Forms.Views;
+
+public partial class LoginForm : Form
+{
+    public LoginForm()
+    {
+        InitializeComponent();
+    }
+
+    private void resgistLbl_Click(object sender, EventArgs e)
+    {
+        var registerForm = new RegisterForm();
+
+        registerForm.FormClosed += (s, args) =>
+        {
+            this.Show();
+        };
+
+        registerForm.Show();
+        this.Hide();
+    }
+
+    private void exitBox_Click(object sender, EventArgs e)
+    {
+        Environment.Exit(0);
+    }
+}
